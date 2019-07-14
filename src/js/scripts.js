b8d668deabd0f6c6fe4cba7cade36761f0a55e16
Bondar Yuriy <bondar.europe@gmail.com>
2019-07-15 00:02:21 +0300

    adds product item page

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -14,6 +14,45 @@ $(".home-carousel").owlCarousel({
   }
 });
 
+$(".product-img-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  navText: [
+    "<span class='icon-arrow-left'>&#9001;</span>",
+    "<span class='icon-arrow-left'>&#9002;</span>"
+  ],
+  responsive: {
+    0: {
+      items: 1
+    },
+    400: {
+      items: 1
+    }
+  }
+});
+
+$(".options-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  navText: [
+    "<span class='icon-arrow-left'>&#9001;</span>",
+    "<span class='icon-arrow-left'>&#9002;</span>"
+  ],
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 3
+    },
+    1200: {
+      items: 4
+    }
+  }
+});
+
 //================================================
 
 $(".navbar-toggler").click(function() {
